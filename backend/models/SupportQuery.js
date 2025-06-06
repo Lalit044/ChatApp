b750d92e8ae429a0e94a1ef19c1f569cda69ab56
Lalit044 <lalitbani044@gmail.com>
2025-06-06 23:23:48 +0530

    Added SupportQuery Mongo Schema

---
+++ backend/models/SupportQuery.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const SupportQuerySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('SupportQuery', SupportQuerySchema);
